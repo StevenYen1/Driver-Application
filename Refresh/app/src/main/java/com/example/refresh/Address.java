0ab6714ef1354079c0ca6093c5f89cf2bbaa8d47
Steven Yen <Steven.Yen@Staples.com>
2019-06-10 12:04:48 -0400

    updated a map activity that will show a map on the application.

--- Refresh/app/src/main/java/com/example/refresh/Address.java
+++ Refresh/app/src/main/java/com/example/refresh/Address.java
@@ -1,9 +1,12 @@
 package com.example.refresh;
 
 import android.app.Dialog;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
 import com.google.android.gms.common.ConnectionResult;
@@ -26,7 +29,14 @@ public class Address extends AppCompatActivity {
     }
 
     private void init(){
-
+        Button openMaps = (Button) findViewById(R.id.openMaps);
+        openMaps.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Address.this, MapActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 
 
