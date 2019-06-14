1696204af58923c1d0a887789b5970e16163d9aa
Yen <Steven.Yen@Staples.com>
2019-06-14 13:55:44 -0400

    WRONG BRANCH: edited the list format of the list of orders

--- Refresh/app/src/main/java/com/example/refresh/Address.java
+++ Refresh/app/src/main/java/com/example/refresh/Address.java
@@ -12,11 +12,15 @@ import android.net.Uri;
 import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Html;
 import android.util.Log;
 import android.view.Gravity;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.FrameLayout;
+import android.widget.ImageView;
 import android.widget.TableLayout;
 import android.widget.TableRow;
 import android.widget.TextView;
@@ -37,6 +41,8 @@ import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
+
+import static android.view.ViewGroup.LayoutParams.MATCH_PARENT;
 
 public class Address extends AppCompatActivity implements Serializable {
 
@@ -83,26 +89,21 @@ public class Address extends AppCompatActivity implements Serializable {
 
 @TargetApi(26)
     private void makeTableHeader(){
+
         tr = new TableRow(this);
+
         TextView status = new TextView(this);
         status.setTypeface(Typeface.MONOSPACE, Typeface.BOLD);
-        status.setText(this.getString(R.string.status));
+        status.setText(this.getString(R.string.status)+"  ");
         status.setAutoSizeTextTypeWithDefaults(TextView.AUTO_SIZE_TEXT_TYPE_UNIFORM);
-        status.setGravity(Gravity.RIGHT);
         tr.addView(status);
 
         TextView order_num = new TextView(this);
         order_num.setText(this.getString(R.string.table_order_num));
         order_num.setTypeface(Typeface.MONOSPACE, Typeface.BOLD);
         order_num.setAutoSizeTextTypeWithDefaults(TextView.AUTO_SIZE_TEXT_TYPE_UNIFORM);
-        order_num.setGravity(Gravity.CENTER);
+//        order_num.setGravity(Gravity.CENTER);
         tr.addView(order_num);
-
-        TextView details = new TextView(this);
-        details.setTypeface(Typeface.MONOSPACE, Typeface.BOLD);
-        details.setText(this.getString(R.string.table_details));
-        details.setAutoSizeTextTypeWithDefaults(TextView.AUTO_SIZE_TEXT_TYPE_UNIFORM);
-        tr.addView(details);
 
         t1.addView(tr);
     }
@@ -116,26 +117,36 @@ public class Address extends AppCompatActivity implements Serializable {
 @TargetApi(26)
     public void addRowFromList() {
         for (Delivery_Item x: list){
+            final Delivery_Item ptr = x;
             tr = new TableRow(this);
+
+
             TextView status = new TextView(this);
-            status.setText(x.getStatus());
             status.setAutoSizeTextTypeWithDefaults(TextView.AUTO_SIZE_TEXT_TYPE_UNIFORM);
-            status.setGravity(Gravity.RIGHT);
+            status.setGravity(Gravity.CENTER);
             if(x.getStatus().equals(this.getString(R.string.Done))){
                 status.setTextColor(0xFF008C00);
+                status.setText("O");
+            }
+            else{
+                status.setText("X");
+                status.setTextColor(0xFFa70000);
             }
             tr.addView(status);
 
             TextView order_num = new TextView(this);
-            order_num.setText(x.getOrderNumber());
+
+            String order_info = String.format(
+                    this.getResources().getString(R.string.orderInfo), x.getOrderNumber(), x.getOrderString());
+
+            order_num.setText(order_info);
+            order_num.setWidth(MATCH_PARENT);
             order_num.setAutoSizeTextTypeWithDefaults(TextView.AUTO_SIZE_TEXT_TYPE_UNIFORM);
-            order_num.setGravity(Gravity.CENTER);
             tr.addView(order_num);
 
-            final Delivery_Item ptr = x;
-            Button details_button = new Button(this);
-            details_button.setText(this.getString(R.string.open));
-            details_button.setOnClickListener(new View.OnClickListener() {
+
+            tr.setBackgroundResource(R.drawable.rowclick);
+            tr.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
                     setData(ptr);
@@ -144,7 +155,8 @@ public class Address extends AppCompatActivity implements Serializable {
                     }
                 }
             });
-            tr.addView(details_button);
+//            tr.setMinimumHeight(100);
+
             t1.addView(tr);
         }
     }
@@ -183,18 +195,18 @@ public class Address extends AppCompatActivity implements Serializable {
 
 
     private void staticList(){
-        Delivery_Item item1 = new Delivery_Item("0100100010", "1600 Pennsylvania Ave NW Washington, DC 20500 ", "President of the US", "How to Tweet 101");
+        Delivery_Item item1 = new Delivery_Item("0100100010", "1600 Pennsylvania Ave NW Washington, DC", "President of the US", "How to Tweet 101");
         Delivery_Item item2 = new Delivery_Item("0002100034", "Seoul, South Korea");
-        Delivery_Item item3 = new Delivery_Item("6112019555", "Champ de Mars, Paris, Ile de France 75007");
-        Delivery_Item item4 = new Delivery_Item("0101010101", "Los Angeles, California, United States");
-        Delivery_Item item5 = new Delivery_Item("5000000000", "50 Vassar St, Cambridge, Massachusetts 02139, United States");
-        Delivery_Item item6 = new Delivery_Item("1231231231", "924 Avenue J East, Grand Prairie, TX 75050");
-        Delivery_Item item7 = new Delivery_Item("5749403-21", "908 Massachusetts Ave, Arlington, MA 02476, United States");
-        Delivery_Item item8 = new Delivery_Item("1-342351-1", "65 Harrison Ave Ste 306, Boston, MA 02111");
-        Delivery_Item item9 = new Delivery_Item("6754456321", "124 Beach St, Ogunquit, ME 03907, United States");
+        Delivery_Item item3 = new Delivery_Item("6112019555", "Champ de Mars, Paris");
+        Delivery_Item item4 = new Delivery_Item("0101010101", "Los Angeles, California");
+        Delivery_Item item5 = new Delivery_Item("5000000000", "50 Vassar St, Cambridge, MA");
+        Delivery_Item item6 = new Delivery_Item("1231231231", "924 Avenue J East, Grand Prairie, TX");
+        Delivery_Item item7 = new Delivery_Item("5749403-21", "908 Massachusetts Ave, Arlington, MA");
+        Delivery_Item item8 = new Delivery_Item("1-342351-1", "65 Harrison Ave Ste 306, Boston, MA");
+        Delivery_Item item9 = new Delivery_Item("6754456321", "124 Beach St, Ogunquit, ME");
         Delivery_Item item10 = new Delivery_Item("9312341129", "〒150-8010東京都渋谷区", "Tetsuya Nomura", "Final Fantasy 7 Remake");
-        Delivery_Item item11 = new Delivery_Item("5512345555", "500 Staples Drive, Framingham, MA 01702", "Saar Picker", "A Hardworking Intern");
-        Delivery_Item item12 = new Delivery_Item("8888888888", "211 Arlington Street, Acton MA 01720");
+        Delivery_Item item11 = new Delivery_Item("5512345555", "500 Staples Drive, Framingham, MA", "Saar Picker", "A Hardworking Intern");
+        Delivery_Item item12 = new Delivery_Item("8888888888", "211 Arlington Street, Acton MA");
 
         ArrayList<Delivery_Item> newList = new ArrayList<Delivery_Item>();
 
@@ -211,22 +223,12 @@ public class Address extends AppCompatActivity implements Serializable {
         newList.add(item11);
         newList.add(item12);
 
-//        for(Delivery_Item x: newList){
-//            Log.d(TAG, "----- " + x.getSignature() + " -----");
-//        }
 
         order_num = this.getIntent().getStringExtra("orderComplete");
 
         completedOrders = this.getIntent().getStringArrayListExtra("completedOrders");
         if(completedOrders != null) {
-//            Log.d(TAG, "-------------------------------------------- "+ readFile(this.getIntent().getStringExtra("signature")) + " --------------------------------------------");
             for (Delivery_Item x : newList){
-//                Log.d("OrderNumber", x.getOrderNumber());
-//                Log.d("orderComplete", this.getIntent().getStringExtra("orderComplete"));
-//                this doesn't actually work because I'm creating the list every freaking time I hope this activity.
-//                if(x.getOrderNumber().equals(this.getIntent().getStringExtra("orderComplete"))){
-//                    x.setSignature(readFile(this.getIntent().getStringExtra("signature")));
-//                }
                 if(completedOrders.contains(x.getOrderNumber())){
                     x.setStatus("Delivered");
                 }
