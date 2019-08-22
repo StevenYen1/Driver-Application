2bbdb80f025570455ce2d609a7059be970859555
Steven Yen <Steven.Yen@Staples.com>
2019-08-22 10:02:22 -0400

    authentication
    Former-commit-id: a21ea89dcb5d95d82068c7d3016e4b7020ad3837

--- Refresh/app/src/main/java/com/example/refresh/Authentication/AccountCreation.java
+++ Refresh/app/src/main/java/com/example/refresh/Authentication/AccountCreation.java
@@ -1,5 +1,15 @@
 package com.example.refresh.Authentication;
+/*
+Description:
+    The purpose of this class is to register new accounts into the local SQLite database.
 
+Specific Features:
+    Inserts new user into "user_table"
+
+Documentation and Code Written By:
+    Steven Yen
+    Staples Intern Summer 2019
+ */
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
@@ -13,6 +23,9 @@ import mehdi.sakout.fancybuttons.FancyButton;
 
 public class AccountCreation extends AppCompatActivity {
 
+    /*
+    Methods that are called when the activity launches.
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,6 +33,9 @@ public class AccountCreation extends AppCompatActivity {
         setSubmitButton();
     }
 
+    /*
+    Setup of the submit button.
+     */
     private void setSubmitButton(){
         EditText username = findViewById(R.id.account_user);
         EditText password = findViewById(R.id.account_password);
@@ -31,27 +47,36 @@ public class AccountCreation extends AppCompatActivity {
             String passInput = password.getText().toString();
             String fNameInput = firstName.getText().toString();
             String lNameInput = lastName.getText().toString();
-
-            if(validInput(userInput)
-                    && validInput(passInput)
-                    && validInput(fNameInput)
-                    && validInput(lNameInput)){
-                DatabaseHelper databaseHelper = new DatabaseHelper(AccountCreation.this);
-                if(!databaseHelper.userExists(userInput)){
-                    databaseHelper.createUser(userInput, passInput, fNameInput, lNameInput);
-                    Intent intent = new Intent(AccountCreation.this, MainActivity.class);
-                    startActivity(intent);
-                }
-                else{
-                    Toast.makeText(AccountCreation.this, "Username unavailable.", Toast.LENGTH_SHORT).show();
-                }
-            }
-            else{
-                Toast.makeText(AccountCreation.this, "Please enter all information.", Toast.LENGTH_SHORT).show();
-            }
+            createNewUser(userInput,passInput,fNameInput,lNameInput);
         });
     }
 
+    /*
+    Creates a new user if all arguments are valid.
+     */
+    private void createNewUser(String userInput, String passInput, String fNameInput, String lNameInput){
+        if(validInput(userInput)
+                && validInput(passInput)
+                && validInput(fNameInput)
+                && validInput(lNameInput)){
+            DatabaseHelper databaseHelper = new DatabaseHelper(AccountCreation.this);
+            if(!databaseHelper.userExists(userInput)){
+                databaseHelper.createUser(userInput, passInput, fNameInput, lNameInput);
+                Intent intent = new Intent(AccountCreation.this, MainActivity.class);
+                startActivity(intent);
+            }
+            else{
+                Toast.makeText(AccountCreation.this, "Username unavailable.", Toast.LENGTH_SHORT).show();
+            }
+        }
+        else{
+            Toast.makeText(AccountCreation.this, "Please enter all information.", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    /*
+    Checks if a string is not empty.
+     */
     private boolean validInput(String input){
         if(!input.equals("")){
             return true;
